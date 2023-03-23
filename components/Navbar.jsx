889d5fee57c79930e1202d480dcbaab5a1e7ea42
Yazdun <yazduntube@gmail.com>
2023-03-23 20:23:06 +0430

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,10 @@
+import { Sidebar } from '.'
+
 export const Navbar = () => {
-  return <div>Navbar</div>
+  return (
+    <div className="p-5 border-b">
+      <Sidebar />
+      <strong>Anime App</strong>
+    </div>
+  )
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import Head from "next/head";
+import Head from 'next/head'
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>hello world</main>
+      <div>Welcome to the Anime APP</div>
     </>
-  );
+  )
 }
