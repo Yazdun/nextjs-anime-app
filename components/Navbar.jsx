b0d1183e8d4ca352e9473e4ee715175264ab80a6
Yazdun <yazduntube@gmail.com>
2023-03-24 21:09:15 +0430

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,14 +2,14 @@ import { Container, Sidebar, ThemeToggle } from '.'
 
 export const Navbar = () => {
   return (
-    <div className="sticky top-0 z-50 bg-white shadow-sm dark:shadow-transparent dark:border-b-2 dark:border-b-gray-800 dark:bg-dark-800">
-      <Container className="flex items-center justify-between" as="nav">
-        <div className="flex items-center gap-2">
-          <Sidebar />
-          <strong>Anime App</strong>
-        </div>
-        <ThemeToggle />
-      </Container>
+    <div className="sticky top-0 z-50 flex items-center justify-between p-5 bg-white shadow-sm dark:shadow-transparent dark:border-b-2 dark:border-b-gray-800 dark:bg-dark-800">
+      {/* <Container className="" as="nav"> */}
+      <div className="flex items-center gap-2">
+        <Sidebar />
+        <strong>Anime App</strong>
+      </div>
+      <ThemeToggle />
+      {/* </Container> */}
     </div>
   )
 }
